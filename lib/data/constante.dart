c92a6fa34b18b5c67e1e8311a866beac55e1f69e
Erwin Fabian Ortega <erwinfabian0222@hotmail.com>
2025-05-19 07:01:04 -0500

    data1

--- lib/data/constante.dart
+++ lib/data/constante.dart
@@ -1,3 +1,3 @@
 const mongoUri =
-    "mongodb+srv://fabian02:<12345>@data1.isgf6zx.mongodb.net/";
+    "mongodb+srv://fabian02:12345@data1.isgf6zx.mongodb.net/?retryWrites=true&w=majority&appName=data1";
 const dbName = "data1";
